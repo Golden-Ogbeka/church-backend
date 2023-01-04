94fb5ae7ff4c7318c27b51ba209007990c400d3a
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2023-01-04 20:07:18 +0100

    Update index.ts
    fixes

--- index.ts
+++ index.ts
@@ -7,7 +7,6 @@ import ApiVersions from './api';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-// const errorHandler = require('./middleware/error')
 
 app.use(cors());
 
@@ -21,30 +20,21 @@ app.use(express.urlencoded({ extended: false }));
 // parse requests of content-type - application/json
 app.use(express.json());
 
-
-
-// simple route
+// base route
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to TFH Admin.' });
 });
 
+// API Routes
 app.use('/api', ApiVersions);
 
+// Not found route
 app.use((req, res) => {
-  res.status(404).json({ message: 'Api not found.' });
+  res.status(404).json({ message: 'API not found.' });
 });
-
-
-//ErrorHandler (Should be last piece of middleware)
-// app.use(errorHandler);
 
 const server = app.listen(
   PORT, () => {
     console.log(`Server is running on port ${PORT}`)
   }
 )
-process.on("unhandledRejection", (error, promise) => {
-  console.log(`Logged Error: ${error}`);
-  server.close(() => process.exit(1))
-
-})
