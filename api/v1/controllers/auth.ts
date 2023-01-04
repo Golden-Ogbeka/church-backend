2d70ecefeacccc29af782286fb7ac6bcdb89d64c
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2023-01-04 22:16:32 +0100

    typescript setup

--- api/v1/controllers/auth.ts
+++ api/v1/controllers/auth.ts
@@ -6,13 +6,16 @@ import express from 'express'
 
 
 export default () => {
+  // interface TypedRequestBody<T> extends Express.Request {
+  //   body: T
+  // }
   // Login an Admin into their Account
-  const Login = async (req: express.Request, res: express.Response) => {
+  const Login = async (req: TypedRequestBody<{ username: string, password: string }>, res: express.Response) => {
     try {
+
       // check for validation errors
       const errors = validationResult(req);
       if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
-
 
       return res.status(200).json({
 
