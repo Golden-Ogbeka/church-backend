29a1903011afc6c751f1b3e66b812a40f5b19fdd
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2023-01-05 21:48:32 +0100

    typescript fix

--- api/v1/controllers/auth.ts
+++ api/v1/controllers/auth.ts
@@ -12,17 +12,19 @@ export default () => {
   //   body: T
   // }
   // Login an Admin into their Account
-  const Login = async (req: TypedRequestBody<{ username: string, password: string }>, res: express.Response) => {
+  const Login = async (req: express.Request<never, never, { email: string, password: string }>, res: express.Response) => {
     try {
 
       // check for validation errors
       const errors = validationResult(req);
       if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
 
+      const { email, password } = req.body
+
       // await UserModel.findone
 
       return res.status(200).json({
-
+        email, password
       });
     } catch (error) {
       return res.status(500).json({ message: 'Internal Server Error' });
