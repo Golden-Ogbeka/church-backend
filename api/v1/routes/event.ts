c66690e5db03107e4428fd2cb0529d1f6cfc663b
Omodamola Oladeji <darmhoo@hotmail.com>
2023-01-17 08:42:29 +0000

    fixed comment

--- api/v1/routes/event.ts
+++ api/v1/routes/event.ts
@@ -37,8 +37,6 @@ router.post(
         body('date', 'Date is required').trim().exists().notEmpty().withMessage("Date cannot be empty").isISO8601().toDate().withMessage("Enter a valid date"),
 
         body('name', 'Name is required').trim().exists().notEmpty().withMessage("Nmae cannot be empty"),
-        body('theme', 'Theme is required').trim().exists().notEmpty().withMessage("Theme cannot be empty"),
-        body('mainText', 'Main Text is required').trim().exists().notEmpty().withMessage("Main Text cannot be empty"),
         body('time', 'Time is required').trim().exists().notEmpty().withMessage("Time cannot be empty"),
         body('allowRegistration', 'Allow Registration is required').trim().exists().isBoolean().withMessage("Allow Registration must be boolean"),
         body('limitedNumberRegistration', 'Limited Number Registration is required').trim().exists().isBoolean().withMessage("Allow Limited Number of Registation must be boolean"),
@@ -48,32 +46,7 @@ router.post(
     EventController.AddEvent
 );
 
-// Update devotional
-// router.patch(
-//     '/',
-//     [
-//         header('x-api-key', 'API Access Denied')
-//             .exists()
-//             .bail()
-//             .custom((value) => isValidAPI(value)),
-//         header('authorization', 'Please specify an authorization header')
-//             .exists()
-//             .bail()
-//             .custom((value) => isAdmin(value)),
-//         body('id', 'ID is required').exists().notEmpty().withMessage("ID cannot be empty").custom(value => isValidObjectId(value)),
-//         body('date', 'Date is required').trim().exists().notEmpty().withMessage("Date cannot be empty").isISO8601().toDate().withMessage("Enter a valid date"),
-//         body('title', 'Title is required').trim().exists().notEmpty().withMessage("Title cannot be empty"),
-//         body('text', 'Text is required').trim().exists().notEmpty().withMessage("Text cannot be empty"),
-//         body('mainText', 'Main Text is required').trim().exists().notEmpty().withMessage("Main Text cannot be empty"),
-//         body('content', 'Content is required').trim().exists().notEmpty().withMessage("Content cannot be empty"),
-//         body('confession', 'Confession is required').trim().exists().notEmpty().withMessage("Confession cannot be empty"),
-//         body('furtherReading', 'Further reading is required').exists().isArray({ min: 1 }).withMessage("Further reading must have at least one scripture"),
-//         body('oneYearBibleReading', 'One year bible reading is required').exists().isArray({ min: 1 }).withMessage("One year bible reading must have at least one scripture"),
-//         body('twoYearsBibleReading', 'Two years bible reading is required').exists().isArray({ min: 1 }).withMessage("Two years bible reading must have at least one scripture"),
 
-//     ],
-//     DevotionalController.UpdateDevotional
-// );
 
 // Get devotional by id
 router.get(
@@ -88,17 +61,7 @@ router.get(
     EventController.ViewEvent
 );
 
-// Get today's devotional
-// router.get(
-//     '/today',
-//     [
-//         header('x-api-key', 'API Access Denied')
-//             .exists()
-//             .bail()
-//             .custom((value) => isValidAPI(value)),
-//     ],
-//     DevotionalController.GetDayDevotional
-// );
+
 
 // Delete devotional by id
 router.delete(
