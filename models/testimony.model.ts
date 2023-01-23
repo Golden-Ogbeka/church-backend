23122f2e4df93bb2738a86932d1976e8cf2f3d59
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2023-01-23 12:22:08 +0100

    Update testimony.model.ts

--- models/testimony.model.ts
+++ models/testimony.model.ts
@@ -2,11 +2,11 @@ import { Schema, model, Document, PaginateModel } from 'mongoose';
 import mongoosePaginate from 'mongoose-paginate-v2';
 
 export interface ITestimony extends Document {
-	fullName: string;
-	summary: string;
-	content: string;
+	fullName: string; // the user's name
+	summary: string; // like: Deliverance from Poverty
+	content: string; // The testimony itself
 	status: string;
-	updatedBy: string;
+	updatedBy: string; // The admin that changes the status
 	source: string; // web or mobile
 }
 
@@ -22,7 +22,7 @@ const testimonySchema = new Schema<ITestimony>(
 			lowercase: true,
 			enum: ['pending', 'approved', 'declined', 'archived'], //these are the allowed statuses
 		},
-		updatedBy: { type: String, required: true },
+		updatedBy: { type: String, required: false },
 		source: { type: String, required: true, enum: ['web', 'mobile'], default: 'web' },
 	},
 	{ timestamps: true }
