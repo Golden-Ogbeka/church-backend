079acde07d4470be5927c5ff2fb96e67e17b4cf7
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2023-01-24 19:38:33 +0100

    add event

--- api/v1/controllers/auth.ts
+++ api/v1/controllers/auth.ts
@@ -1,63 +1,74 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 import { validationResult } from 'express-validator';
-import express from 'express'
+import express from 'express';
 import AdminModel from '../../../models/admin.model';
 import { sendEmail } from '../../../utils/mailer';
-import crypto from "crypto"
-
+import crypto from 'crypto';
 
 export default () => {
   // Login an Admin into their Account
-  const Login = async (req: express.Request<never, never, { email: string, password: string }>, res: express.Response) => {
+  const Login = async (
+    req: express.Request<never, never, { email: string; password: string }>,
+    res: express.Response
+  ) => {
     try {
       // check for validation errors
       const errors = validationResult(req);
       if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
 
-      const { email, password } = req.body
+      const { email, password } = req.body;
 
       // find user
       const existingUser = await AdminModel.findOne({ email });
-      if (!existingUser) return res.status(401).json({ message: "Invalid email or password" })
+      if (!existingUser)
+        return res.status(400).json({ message: 'Invalid email or password' });
 
       // compare passwords
       bcrypt.compare(password, existingUser.password, function (err, matched) {
-        if (!matched) return res.status(401).json({ message: "Invalid email or password" })
+        if (!matched)
+          return res.status(400).json({ message: 'Invalid email or password' });
 
         // Generate JWT Token
-        jwt.sign(existingUser.toJSON(), process.env.JWT_SECRET || "secret", { expiresIn: "2d" }, (err, token) => {
-          return res.status(200).json({
-            message: "Login successful",
-            user: existingUser,
-            token
-          });
-        })
+        jwt.sign(
+          existingUser.toJSON(),
+          process.env.JWT_SECRET || 'secret',
+          { expiresIn: '2d' },
+          (err, token) => {
+            return res.status(200).json({
+              message: 'Login successful',
+              user: existingUser,
+              token,
+            });
+          }
+        );
       });
     } catch (error) {
       return res.status(500).json({ message: 'Internal Server Error' });
     }
   };
 
-
   interface RegistrationDetails {
     fullname: string;
     email: string;
     password: string;
-
   }
 
-  const Register = async (req: express.Request<never, never, RegistrationDetails>, res: express.Response) => {
+  const Register = async (
+    req: express.Request<never, never, RegistrationDetails>,
+    res: express.Response
+  ) => {
     try {
       // check for validation errors
       const errors = validationResult(req);
       if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
 
-      const { email, password, fullname } = req.body
+      const { email, password, fullname } = req.body;
 
       // check if user exists
       let existingUser = await AdminModel.findOne({ email });
-      if (existingUser && Object.keys(existingUser).length) return res.status(401).json({ message: "User already exists" });
+      if (existingUser && Object.keys(existingUser).length)
+        return res.status(400).json({ message: 'User already exists' });
 
       // Hash password
       bcrypt.hash(password, 8, async function (err, hash) {
@@ -67,12 +78,12 @@ export default () => {
           email,
           password: hash,
           fullname,
-        })
-
-        return res.status(200).json({
-          message: "User created successfully", user: newUser
         });
 
+        return res.status(200).json({
+          message: 'User created successfully',
+          user: newUser,
+        });
       });
     } catch (error) {
       return res.status(500).json({ message: 'Internal Server Error' });
@@ -80,18 +91,20 @@ export default () => {
   };
 
   // Request Password Reset
-  const ResetPasswordRequest = async (req: express.Request<never, never, { email: string }>, res: express.Response) => {
+  const ResetPasswordRequest = async (
+    req: express.Request<never, never, { email: string }>,
+    res: express.Response
+  ) => {
     try {
-
       // check for validation errors
       const errors = validationResult(req);
       if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
 
-      const { email } = req.body
+      const { email } = req.body;
 
       // check if user exists
       let existingUser = await AdminModel.findOne({ email });
-      if (!existingUser) return res.status(404).json({ message: "User not found" })
+      if (!existingUser) return res.status(404).json({ message: 'User not found' });
 
       const verificationCode = crypto.randomUUID().substring(0, 5);
 
@@ -100,57 +113,62 @@ export default () => {
       existingUser.save();
 
       const emailToSend = {
-				body: {
-					greeting: 'Dear',
-					name: existingUser?.fullname,
-					intro:
-						'You have received this email because a password reset request for your account was received.',
-					action: {
-						instructions: `Click the button below to reset your password or use <b>${verificationCode}</b> as your verification code :`,
-						button: {
-							color: '#DC4D2F',
-							text: 'Reset password',
-							fallback: true,
-							link: `${
-								process.env.ADMIN_DASHBOARD_URL || ''
-							}/reset-password/update/${verificationCode}`,
-						},
-					},
-					signature: 'Regards',
-					outro:
-						'If you did not request a password reset, no further action is required on your part.',
-				},
-			};
+        body: {
+          greeting: 'Dear',
+          name: existingUser?.fullname,
+          intro:
+            'You have received this email because a password reset request for your account was received.',
+          action: {
+            instructions: `Click the button below to reset your password or use <b>${verificationCode}</b> as your verification code :`,
+            button: {
+              color: '#DC4D2F',
+              text: 'Reset password',
+              fallback: true,
+              link: `${
+                process.env.ADMIN_DASHBOARD_URL || ''
+              }/reset-password/update/${verificationCode}`,
+            },
+          },
+          signature: 'Regards',
+          outro:
+            'If you did not request a password reset, no further action is required on your part.',
+        },
+      };
 
-      sendEmail(email, "Reset Password", emailToSend)
+      sendEmail(email, 'Reset Password', emailToSend);
 
       return res.status(200).json({
-        message: "Reset password request successful",
+        message: 'Reset password request successful',
       });
-
     } catch (error) {
       return res.status(500).json({ message: 'Internal Server Error' });
     }
   };
 
-  const ResetPasswordUpdate = async (req: express.Request<never, never, { email: string, newPassword: string, verificationCode: string }>, res: express.Response) => {
+  const ResetPasswordUpdate = async (
+    req: express.Request<
+      never,
+      never,
+      { email: string; newPassword: string; verificationCode: string }
+    >,
+    res: express.Response
+  ) => {
     try {
-
       // check for validation errors
       const errors = validationResult(req);
       if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
 
-      const { email, newPassword, verificationCode } = req.body
+      const { email, newPassword, verificationCode } = req.body;
 
       // check if user exists
       let existingUser = await AdminModel.findOne({ email, verificationCode });
 
       // Hash password
       bcrypt.hash(newPassword, 8, async function (err, hash) {
-        if (!existingUser) return res.status(401).json({ message: "Invalid email or verification code" });
+        if (!existingUser)
+          return res.status(400).json({ message: 'Invalid email or verification code' });
 
         const verificationCode = crypto.randomUUID().substring(0, 5);
-
 
         existingUser.password = hash;
         existingUser.verificationCode = verificationCode; //resetting the verification code also
@@ -161,15 +179,15 @@ export default () => {
             greeting: 'Dear',
             name: existingUser?.fullname,
             intro: 'You have successfully reset your password',
-            signature: "Regards",
-            outro: 'If you did not request a password reset, please contact TFH Admin.'
-          }
+            signature: 'Regards',
+            outro: 'If you did not request a password reset, please contact TFH Admin.',
+          },
         };
 
-        sendEmail(email, "Reset password update", emailToSend)
+        sendEmail(email, 'Reset password update', emailToSend);
 
         return res.status(200).json({
-          message: "Password updated successfully",
+          message: 'Password updated successfully',
         });
       });
     } catch (error) {
@@ -177,11 +195,10 @@ export default () => {
     }
   };
 
-
   return {
     Login,
     Register,
     ResetPasswordRequest,
-    ResetPasswordUpdate
+    ResetPasswordUpdate,
   };
 };
--- models/event.model.ts
+++ models/event.model.ts
@@ -1,5 +1,5 @@
 import { UncertainObjectType } from './../types/index';
-import { Schema, model, Document, PaginateModel } from "mongoose";
+import { Schema, model, Document, PaginateModel } from 'mongoose';
 import mongoosePaginate from 'mongoose-paginate-v2';
 
 export interface IEvent extends Document {
@@ -22,32 +22,37 @@ export interface IEvent extends Document {
   updatedBy: string;
 }
 
-
-const eventSchema = new Schema<IEvent>({
-  name: { type: String, required: true },
-  theme: { type: String, required: false },
-  mainText: { type: String, required: false },
-  date: { type: String, required: true },
-  time: { type: String, required: true },
-  allowRegistration: { type: Boolean, required: true, default: false },
-  registrationEntries: { type: [], required: false },
-  requiredRegistrationDetails: { type: [], required: false },
-  gallery: { type: [String], required: false },
-  limitedNumberRegistration: { type: Boolean, required: true, default: false },
-  registrationNumberLimit: { type: Number, required: false },
-  limitedDateRegistration: { type: Boolean, required: true, default: false },
-  registrationDateLimit: { type: Date, required: false },
-  poster: { type: String, required: false },
-  createdBy: { type: String, required: true },
-  updatedBy: { type: String, required: true },
-}, { timestamps: true });
+const eventSchema = new Schema<IEvent>(
+  {
+    name: { type: String, required: true },
+    theme: { type: String, required: false },
+    mainText: { type: String, required: false },
+    date: { type: String, required: true },
+    time: { type: String, required: true },
+    allowRegistration: { type: Boolean, required: true, default: false },
+    registrationEntries: { type: [], required: false },
+    requiredRegistrationDetails: {
+      type: [],
+      required: false,
+    },
+    gallery: { type: [String], required: false },
+    limitedNumberRegistration: { type: Boolean, required: true, default: false },
+    registrationNumberLimit: { type: Number, required: false },
+    limitedDateRegistration: { type: Boolean, required: true, default: false },
+    registrationDateLimit: { type: Date, required: false },
+    poster: { type: String, required: false },
+    createdBy: { type: String, required: true },
+    updatedBy: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 eventSchema.plugin(mongoosePaginate);
 
 // declare a mongoose document based on a Typescript interface representing your schema
 
-const EventModel = model<IEvent, PaginateModel<IEvent>>('Event', eventSchema)
+const EventModel = model<IEvent, PaginateModel<IEvent>>('Event', eventSchema);
 
 // EventModel.paginate
 
-export default EventModel
+export default EventModel;
