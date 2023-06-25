e46ce5f870871087b63c5b890adb194f0eb3bae4
Golden-Ogbeka <57953557+Golden-Ogbeka@users.noreply.github.com>
2023-06-25 18:53:45 +0100

    testimony api v2

--- api/v2/controllers/testimony.ts
+++ api/v2/controllers/testimony.ts
@@ -1,10 +1,13 @@
-import { getUserDetails } from './../../../functions/auth'
-import { TestimonyType } from './../../../types/index'
-import { getPaginationOptions } from './../../../utils/pagination'
-import { validationResult } from 'express-validator'
-import express from 'express'
-import TestimonyModel, { ITestimony } from '../../v1/models/testimony.model';
-import { getDateFilters } from '../../../functions/filters'
+import { TestimonyType } from './../../../types/index';
+import { getPaginationOptions } from './../../../utils/pagination';
+import { validationResult } from 'express-validator';
+import express from 'express';
+import {
+  getDateFilters,
+  getSequelizeDateFilters,
+} from '../../../functions/filters';
+import { TestimonyModel } from '../models/testimony';
+import { getResponseVariables, paginate } from '../../../functions/pagination';
 
 export default () => {
   const GetAllTestimonies = async (
@@ -22,25 +25,28 @@ export default () => {
       if (!errors.isEmpty())
         return res.status(422).json({ errors: errors.array() });
 
-      const paginationOptions = getPaginationOptions(req as any);
+      const { from, to, limit, page } = req.query;
 
       const { status } = req.body;
 
       // find all testimonies
 
       const testimoniesData = status
-        ? await TestimonyModel.paginate(
-            { ...getDateFilters(req as any), status },
-            paginationOptions
-          )
-        : await TestimonyModel.paginate(
-            getDateFilters(req as any),
-            paginationOptions
-          );
+        ? await TestimonyModel.findAndCountAll({
+            where: { status },
+            order: [['ditto', 'DESC']],
+            ...getSequelizeDateFilters({ from, to }),
+            ...paginate({ limit, page }),
+          })
+        : await TestimonyModel.findAndCountAll({
+            order: [['ditto', 'DESC']],
+            ...getSequelizeDateFilters({ from, to }),
+            ...paginate({ limit, page }),
+          });
 
       return res.status(200).json({
         message: 'All Testimonies Retrieved',
-        data: testimoniesData,
+        data: getResponseVariables(testimoniesData, limit),
       });
     } catch (error: any) {
       return res
@@ -48,43 +54,6 @@ export default () => {
         .json({ message: error?.message || 'Internal Server Error' });
     }
   };
-
-  // const GetTestimonyByStatus = async (
-  //   req: express.Request<
-  //     never,
-  //     never,
-  //     never,
-  //     { page: number; limit: number; from: string; to: string; status: string }
-  //   >,
-  //   res: express.Response
-  // ) => {
-  //   try {
-  //     // check for validation errors
-  //     const errors = validationResult(req)
-  //     if (!errors.isEmpty())
-  //       return res.status(422).json({ errors: errors.array() })
-
-  //     const { page = 1, limit = 10, status } = req.query
-
-  //     const testimonies = await TestimonyModel.find({
-  //       status,
-  //     })
-  //       .limit(limit * 1)
-  //       .skip((page - 1) * limit)
-  //       .exec()
-
-  //     const count = await TestimonyModel.find({ status }).count()
-
-  //     return res.status(200).json({
-  //       message: `All ${status} testimonies retrieved`,
-  //       data: testimonies,
-  //       totalPages: Math.ceil(count / limit),
-  //       curentPage: page,
-  //     })
-  //   } catch (error) {
-  //     return res.status(500).json({ message: 'Internal Server Error' })
-  //   }
-  // }
 
   const ViewTestimony = async (
     req: express.Request<{ id: string }>,
@@ -100,7 +69,7 @@ export default () => {
 
       // find testimony
 
-      const testimonyData = await TestimonyModel.findById(id);
+      const testimonyData = await TestimonyModel.findByPk(id);
 
       if (!testimonyData)
         return res.status(404).json({ message: 'Testimony not found' });
@@ -116,6 +85,18 @@ export default () => {
     }
   };
 
+  interface TestimonyType {
+    email: string;
+    phoneNumber: string;
+    fullName: string;
+    summary: string;
+    content: string;
+    status: 'pending' | 'approved' | 'declined' | 'archived';
+    source: 'web' | 'mobile';
+    updatedBy: string;
+    _id: string;
+  }
+
   const AddTestimony = async (
     req: express.Request<never, never, TestimonyType>,
     res: express.Response
@@ -128,10 +109,10 @@ export default () => {
       const { email, content, fullName, summary, source, phoneNumber } =
         req.body;
 
-      const testimonyData = new TestimonyModel({
-        fullName,
-        content,
-        summary,
+      const testimonyData = await TestimonyModel.create({
+        names: fullName,
+        main_gist: content,
+        titles: summary,
         source,
         email,
         phoneNumber,
@@ -159,7 +140,7 @@ export default () => {
 
       const { id } = req.params;
 
-      const existingTestimony = await TestimonyModel.findById(id);
+      const existingTestimony = await TestimonyModel.findByPk(id);
       if (!existingTestimony)
         return res.status(404).json({ message: 'Testimony not found' });
 
@@ -171,12 +152,14 @@ export default () => {
         message: 'Testimony status updated successfully',
         testimony: existingTestimony,
       });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ message: 'Internal Server Error' });
+    } catch (error: any) {
+      return res
+        .status(500)
+        .json({ message: error?.message || 'Internal Server Error' });
     }
   };
 
+  // This is the one users call
   const GetApprovedTestimonies = async (
     req: express.Request<
       never,
@@ -192,17 +175,19 @@ export default () => {
       if (!errors.isEmpty())
         return res.status(422).json({ errors: errors.array() });
 
-      const paginationOptions = getPaginationOptions(req as any);
+      const { from, to, limit, page } = req.query;
 
       // find all approved testimonies
 
-      const testimoniesData = await TestimonyModel.paginate(
-        { ...getDateFilters(req as any), status: 'approved' },
-        paginationOptions
-      );
+      const testimoniesData = await TestimonyModel.findAndCountAll({
+        where: { status: 'approved' },
+        order: [['ditto', 'DESC']],
+        ...getSequelizeDateFilters({ from, to }),
+        ...paginate({ limit, page }),
+      });
       return res.status(200).json({
         message: 'Testimonies Retrieved',
-        data: testimoniesData,
+        data: getResponseVariables(testimoniesData, limit),
       });
     } catch (error: any) {
       return res
@@ -217,5 +202,5 @@ export default () => {
     ChangeStatus,
     ViewTestimony,
     GetApprovedTestimonies,
-  }
-}
+  };
+};
